9d805af4aa6b114026233acfa3e6b3c12226b5b3
TylerTHChen <tchen456@wisc.edu>
2026-03-10 16:21:17 -0500

    ob_opb ready for test

--- src/ob_opb.sv
+++ src/ob_opb.sv
@@ -1,10 +1,10 @@
 import ob_pkg::*;
-module ob_opb opb(
+module ob_opb(
     input logic i_clk,
     input logic i_rst_n,
     input logic [ORDERID_LEN-1:0] i_order_id,
-    input logic [QUATITY_LEN-1:0] i_quantity,
-    input logic i_action,
+    input logic [QUANTITY_LEN-1:0] i_quantity,
+    input logic [1:0] i_action,
     input logic i_valid,
     input logic [PRICE_LEN-1:0] i_price,
     output logic o_add,
@@ -14,46 +14,110 @@ module ob_opb opb(
 ); 
 
 // OPB: use orderid as index and store the price and quantity of the given 
-(* ram_style = "block" *) logic [W-1:0] OPB [0:DEPTH-1];
+(* ram_style = "block" *) ob_packet_t OPB [0:DEPTH-1];
 
 // this table track tif each entry is valid (purpose: not reset bram when rst_n)
 logic valid_table[0:DEPTH-1];
 
 
-// current packet being fed (only used when it is an add order)
+// packet being fed from prev cycle (only used when it is an add order)
 ob_packet_t packet_in;
-assign packet_in = '{price:i_price, quantity:i_quantity};
 
 // action status
-logic add, cancel, execute;
-assign add = i_action == 2'b00;
-assign cancel = i_action = 2'b01;
-assign execute = i_action = 2'b10;
+logic add, cancel, execute, delete;
+assign add = (i_action == 2'b00);
+assign cancel = (i_action == 2'b01);
+assign execute = (i_action == 2'b10);
+assign delete = (i_action == 2'b11);
 
 // updated packet if it is the given orderid is canceled or executed
+ob_packet_t                 packet_out;
+logic [QUANTITY_LEN-1:0]    quantity_to_remove;
+
+//pipeline vars
+logic [QUANTITY_LEN-1:0]    p_quantity;
+logic [ORDERID_LEN-1:0]     p_order_id;
+logic                       p_add, p_exec_cancel, p_delete;
+
+
+// pipeline orderid and quantity
+always_ff @(posedge i_clk, negedge i_rst_n) begin
+    if(!i_rst_n) begin
+        p_quantity <= '0;
+        p_order_id <= '0;
+    end else begin
+        p_quantity <= i_quantity;
+        p_order_id <= i_order_id;
+    end
+end
+
 
 // add order
 always_ff @(posedge i_clk, negedge i_rst_n) begin
-    if(!rst_n) begin
-    end
-    else if(add && i_valid) begin
-        OPB[i_order_id] <= packet_in;
+    if(!i_rst_n) begin
+        p_add <= 0;
+    end else begin
+        p_add <= 0;
+        if(add && i_valid) begin
+            p_add <= 1;
+            packet_in <= '{price:i_price, quantity:i_quantity};
+        end
+        if(p_add)
+            OPB[p_order_id] <= packet_in;
     end
 end
 
-
-
-// execute order
+// cancel and execute order
 always_ff @(posedge i_clk, negedge i_rst_n) begin
     if(!i_rst_n) begin
-    end
-    else if(execute && i_valid && valid_table[i_order_id] == 1) begin
-        if(OPB[i_order_id].quantity > i_quantity) begin
-            OPB[i_order_id].quantity <= OPB[i_order_id].quantity - i_quantity;  // todo: parallel this since now read/write bram happen in the same cycle which violated it property
-        end else begin
-            OPB[i_order_id].quantity <= '0;
+        p_exec_cancel <= 0;
+        quantity_to_remove <= '0;
+    end else begin
+        p_exec_cancel <= 0;
+        if((cancel || execute) && i_valid && valid_table[i_order_id]) begin
+            packet_out <= OPB[i_order_id];
+            p_exec_cancel <= 1;
+        end
+        if (p_exec_cancel) begin
+            if((cancel || execute) && (p_order_id == i_order_id)) begin
+                quantity_to_remove <= quantity_to_remove + p_quantity;
+            end else begin
+                if (packet_out.quantity <= (quantity_to_remove + p_quantity)) begin
+                    OPB[p_order_id].quantity <= '0;
+                end else begin
+                    OPB[p_order_id].quantity <= packet_out.quantity - quantity_to_remove - p_quantity;
+                end
+            end
         end
     end
 end
+
+// delete order (and valid table)
+always_ff @(posedge i_clk, negedge i_rst_n) begin
+    if(!i_rst_n) begin
+        p_delete <= 0;
+    end else begin
+        p_delete <= 0;
+        if(delete && i_valid && valid_table[i_order_id]) begin
+            p_delete <= 1;
+        end
+    end
+end
+
+// valid table
+always_ff @(posedge i_clk, negedge i_rst_n) begin
+    if(!i_rst_n)
+        valid_table <= '0;
+    else begin
+        if(p_delete) begin
+            valid_table[p_order_id] <= 0;
+        end else if(p_exec_cancel && (packet_out.quantity <= (quantity_to_remove + p_quantity))) begin
+            valid_table[p_order_id] <= 0;
+        end else if(p_add) begin
+            valid_table[p_order_id] <= 1;
+        end
+    end
+end
+
 
 endmodule
