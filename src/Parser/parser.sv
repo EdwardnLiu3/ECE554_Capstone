451efbdc9f390c9a343f132401398993063fb698
U-ENGR\wastrong <wastrong@win-8106.ad.engr.wisc.edu>
2026-04-14 16:21:56 -0500

    updated parser to include timestamp

--- src/Parser/parser.sv
+++ src/Parser/parser.sv
@@ -11,7 +11,8 @@ module parser
     output  [PRICE_LEN-1:0]             o_price,
     output  [1:0]                       o_action,
     output                              o_valid,
-    output  [STOCK_LEN-1:0]             o_stock_id
+    output  [STOCK_LEN-1:0]             o_stock_id,
+    output  [47:0]                      o_timestamp
 );
 
 
@@ -22,7 +23,7 @@ reg [PRICE_LEN-1:0] price_ff;
 reg [1:0] action_ff;
 reg valid_ff;
 reg [STOCK_LEN-1:0] stock_id_ff;
-
+reg [47:0] timestamp_ff;
 
 assign o_order_id = order_id_ff;
 assign o_quantity = quantity_ff;
@@ -31,6 +32,7 @@ assign o_price = price_ff;
 assign o_action = action_ff;
 assign o_valid = valid_ff;
 assign o_stock_id = stock_id_ff;
+assign o_timestamp = timestamp_ff;
 
 always_ff @(posedge i_clk, negedge i_rst_n) begin
     if(!i_rst_n) begin
@@ -40,7 +42,8 @@ always_ff @(posedge i_clk, negedge i_rst_n) begin
         price_ff <= '0;
         action_ff <= '0;
         valid_ff <= '0;
-        stock_id_ff <= '0;       
+        stock_id_ff <= '0;
+        timestamp_ff <= '0;       
     end
     else if(i_payload[7:0] == 8'b01000001) begin //add
         order_id_ff <= i_payload[97:88];
@@ -50,6 +53,7 @@ always_ff @(posedge i_clk, negedge i_rst_n) begin
         action_ff <= 2'b00;
         valid_ff <= i_valid;
         stock_id_ff <= i_payload[23:8];
+        timestamp_ff <= i_payload[87:40];
     end
     else if(i_payload[7:0] == 8'b01011000) begin //cancel
         order_id_ff <= i_payload[97:88];
@@ -59,6 +63,7 @@ always_ff @(posedge i_clk, negedge i_rst_n) begin
         action_ff <= 2'b01;
         valid_ff <= i_valid;
         stock_id_ff <= i_payload[23:8];
+        timestamp_ff <= i_payload[87:40];
     end
     else if(i_payload[7:0] == 8'b01000100) begin //delete
         order_id_ff <= i_payload[97:88];
@@ -68,6 +73,7 @@ always_ff @(posedge i_clk, negedge i_rst_n) begin
         action_ff <= 2'b11;
         valid_ff <= i_valid;
         stock_id_ff <= i_payload[23:8];
+        timestamp_ff <= i_payload[87:40];
     end
     else if(i_payload[7:0] ==  8'b01000101) begin //execute
         order_id_ff <= i_payload[97:88];
@@ -77,6 +83,7 @@ always_ff @(posedge i_clk, negedge i_rst_n) begin
         action_ff <= 2'b10;
         valid_ff <= i_valid;
         stock_id_ff <= i_payload[23:8];
+        timestamp_ff <= i_payload[87:40];
     end
     else begin
         order_id_ff <= '0;
@@ -86,6 +93,7 @@ always_ff @(posedge i_clk, negedge i_rst_n) begin
         action_ff <= '0;
         valid_ff <= 1'b0;
         stock_id_ff <= '0;
+        timestamp_ff <= '0;
     end
 end 
 
